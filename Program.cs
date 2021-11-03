fdb5353058b4d9d066cb686685e6f3d7b82eface
jacksoncooper12 <jacksoncooper12@gmail.com>
2021-11-02 23:01:30 -0400

    removed await

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using PGNScraper;
 
 namespace PGNScraper
 {
@@ -17,6 +21,7 @@ namespace PGNScraper
         {
             Console.WriteLine("Please input a Chess.com username and press enter: ");
             var username = Console.ReadLine();
+            var now = DateTime.Now;
             var initialURL = string.Format("https://api.chess.com/pub/player/{0}/games/archives", username);
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(initialURL);
@@ -35,53 +40,68 @@ namespace PGNScraper
             {
                 using (FileStream fs = File.Create(string.Format("{0}{1}", KnownFolders.GetPath(KnownFolder.Downloads), string.Format(@"\{0}.pgn", username))))
                 {
-
+                    var tasks = new List<Task>();
                     foreach (var child in jObj["archives"].Children())
                     {
-                        var yearMonth = child.ToString().Substring(Math.Max(0, child.ToString().Length - 7));
-                        var month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(Int32.Parse(yearMonth.Substring(Math.Max(0, yearMonth.Length - 2))));
-                        var year = yearMonth.Substring(0, 4);
-                        Console.Write(string.Format("\rGathering {0} monthly games: {1}   ", username, string.Format("{0} {1}", year, month)));
-                        HttpClient innerClient = new HttpClient();
-                        innerClient.BaseAddress = new Uri(child.ToString());
-                        innerClient.DefaultRequestHeaders.Accept.Add(
-                        new MediaTypeWithQualityHeaderValue("application/json"));
-                        HttpResponseMessage innerResponse = innerClient.GetAsync(child.ToString()).Result;
-                        var innerStringResponse = await innerResponse.Content.ReadAsStringAsync();
-                        JObject innerJObj = JObject.Parse(innerStringResponse);
-                        foreach (var game in innerJObj["games"].Children())
-                        {
-                            if (game.ToString().Contains("pgn"))
-                            {
-                                var pgn = game["pgn"].ToString();
-                                foreach (var possiblePly in plycounts)
-                                {
-                                    if (pgn.Contains(possiblePly))
-                                    {
-                                        var moveCount = int.Parse(possiblePly.Remove(possiblePly.IndexOf('.')));
-                                        var plyCount = moveCount * 2;
-                                        pgn = string.Format("[PlyCount \"{0}\"]\r\n{1}", plyCount, pgn);
-                                        break;
-                                    }
-                                }
-                                Byte[] pgnBytes = new UTF8Encoding(true).GetBytes(pgn);
-                                fs.Write(pgnBytes, 0, pgnBytes.Length);
-                                gameCount++;
-                            }
-                        }
+                        KnownFolders.Add(child, username, plycounts, fs, gameCount);
                     }
-                    Console.WriteLine(string.Format("\nDone! Your PGN has been saved to your Downloads folder. ({0:n0} total games)", gameCount));
-                    Console.ReadLine();
-                }
+                };
             }
             else
             {
                 Console.WriteLine(string.Format("No Chess.com users found matching '{0}'", username));
             }
             client.Dispose();
+            Console.WriteLine(string.Format("\r\n{0} seconds to complete\r\nPress any key to exit", (DateTime.Now - now).TotalSeconds));
+            var wait = Console.ReadLine();
         }
+
         public static class KnownFolders
         {
+            public static async Task Add(JToken child, string username, List<string> plycounts, FileStream fs, int gameCount)
+            {
+                var yearMonth = child.ToString().Substring(Math.Max(0, child.ToString().Length - 7));
+                var month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(Int32.Parse(yearMonth.Substring(Math.Max(0, yearMonth.Length - 2))));
+                var year = yearMonth.Substring(0, 4);
+                Console.Write(string.Format("\rGathering {0} monthly games: {1}   ", username, string.Format("{0} {1}", year, month)));
+                HttpClient innerClient = new HttpClient();
+                innerClient.BaseAddress = new Uri(child.ToString());
+                innerClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage innerResponse = innerClient.GetAsync(child.ToString()).Result;
+                var innerStringResponse = await innerResponse.Content.ReadAsStringAsync();
+                JObject innerJObj = JObject.Parse(innerStringResponse);
+                foreach (var game in innerJObj["games"].Children())
+                {
+                    if (game.ToString().Contains("pgn"))
+                    {
+                        var pgn = game["pgn"].ToString();
+                        foreach (var possiblePly in plycounts)
+                        {
+                            if (pgn.Contains(possiblePly))
+                            {
+                                var moveCount = int.Parse(possiblePly.Remove(possiblePly.IndexOf('.')));
+                                var plyCount = moveCount * 2;
+                                pgn = string.Format("[PlyCount \"{0}\"]\r\n{1}", plyCount, pgn);
+                                break;
+                            }
+                        }
+                        var dfg = 1;
+
+                        if (pgn.Contains("Nikolas_Pushkin"))
+                        {
+                            dfg++;
+
+                        }
+                        Byte[] pgnBytes = new UTF8Encoding(true).GetBytes(pgn);
+                        fs.Write(pgnBytes, 0, pgnBytes.Length);
+                        gameCount++;
+                    }
+                }
+            }
+
+
+
             private static string[] _knownFolderGuids = new string[]
             {
                 "{374DE290-123F-4565-9164-39C4925E467B}", // Downloads
